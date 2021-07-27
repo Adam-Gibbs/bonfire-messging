3aea6c33987c9f808b226a604dfb26d9a2c9d8ed
Adam Gibbs <adam-gibbs@outlook.com>
2021-07-27 23:53:03 +0100

    try again

--- general/general.py
+++ general/general.py
@@ -23,38 +23,37 @@ else:
     client = boto3.client('dynamodb')
 
 def get_user(event, context):
-    print(event)
-    print(context)
-    # resp = client.get_item(
-    #     TableName=USERS_TABLE,
-    #     Key={
-    #         'userId': { 'S': user_id }
-    #     }
-    # )
-    # item = resp.get('Item')
-    # if not item:
-    #     return jsonify({'error': 'User does not exist'}), 404
-
-    # return jsonify({
-    #     'userId': item.get('userId').get('S'),
-    #     'name': item.get('name').get('S')
-    # })
-
-def create_user(event, context):
-    user_id = request.json.get('userId')
-    name = request.json.get('name')
-    if not user_id or not name:
-        return jsonify({'error': 'Please provide userId and name'}), 400
-
-    client.put_item(
+    resp = client.get_item(
         TableName=USERS_TABLE,
-        Item={
-            'userId': {'S': user_id },
-            'name': {'S': name }
+        Key={
+            'userId': { 'S': event.pathParameters.user_id }
         }
     )
+    item = resp.get('Item')
+    if not item:
+        return jsonify({'error': 'User does not exist'}), 404
 
     return jsonify({
-        'userId': user_id,
-        'name': name
+        'userId': item.get('userId').get('S'),
+        'name': item.get('name').get('S')
     })
+
+def create_user(event, context):
+    print(event)
+    # user_id = request.json.get('userId')
+    # name = request.json.get('name')
+    # if not user_id or not name:
+    #     return jsonify({'error': 'Please provide userId and name'}), 400
+
+    # client.put_item(
+    #     TableName=USERS_TABLE,
+    #     Item={
+    #         'userId': {'S': user_id },
+    #         'name': {'S': name }
+    #     }
+    # )
+
+    # return {
+    #     'userId': user_id,
+    #     'name': name
+    # }
