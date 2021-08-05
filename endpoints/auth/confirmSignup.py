620a8154a62ca7f43a4d84f55e5123818f879cf0
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-05 23:38:57 +0100

    retreive parms

--- endpoints/auth/confirmSignup.py
+++ endpoints/auth/confirmSignup.py
@@ -4,11 +4,12 @@ USER_POOL_ID = 'eu-west-2_xUNInjJwa'
 CLIENT_ID = '8gjhsrum4alfi1u9i6prargi2'
 
 def lambda_handler(event, context):
+    params = event.get("body")
     client = boto3.client('cognito-idp')
     
     try:
-        username = event['username']
-        code = event['code']
+        username = params['username']
+        code = params['code']
         
         response = client.confirm_sign_up(
             ClientId=CLIENT_ID,
@@ -20,8 +21,8 @@ def lambda_handler(event, context):
         print(response)
 
     except client.exceptions.UserNotFoundException:
-        # return {"success": False, "message": "Username doesnt exists"}
-        return event
+        return {"success": False, "message": "Username doesn't exists"}
+        # return event
         
     except client.exceptions.CodeMismatchException:
         return {"success": False, "message": "Invalid Verification code"}
--- endpoints/auth/signup.py
+++ endpoints/auth/signup.py
@@ -5,18 +5,17 @@ USER_POOL_ID = 'eu-west-2_xUNInjJwa'
 CLIENT_ID = '8gjhsrum4alfi1u9i6prargi2'
 
 def lambda_handler(event, context):
-	print(event)
-	print("\n\n\n")
+	params = event.get("body")
 	for field in ["username", "email", "password"]:
 		print(f"checking {field}...")
-		if not event.get(field):
+		if not params.get(field):
 			print("failed")
 			generate_response(400, {"success": False, 'message': f"{field} is not present", "data": None})
 		print("success")
 
-	username = event['username']
-	email = event["email"]
-	password = event['password']
+	username = params['username']
+	email = params["email"]
+	password = params['password']
 	print(f"username: {username}, email: {email}, password: {password}")
 
 	client = boto3.client('cognito-idp')
