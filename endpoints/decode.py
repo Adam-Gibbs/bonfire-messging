da496592a88959d0b0353d9ab2c0e3ddca8e06ed
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-06 23:31:39 +0100

    redo decode

--- endpoints/decode.py
+++ endpoints/decode.py
@@ -1,13 +1,17 @@
-import requests
-from jose import jwt
-from pprint import pprint
+import cognitojwt
 from endpoints.helpers.getData import get_header_auth
 from endpoints.helpers.returns import generate_response
 import endpoints.helpers.config as config
 
 
 def decode_token(token):
-    return jwt.decode(token)
+    return cognitojwt.decode(
+        token,
+        config.USER_POOL_LOC,
+        config.USER_POOL_ID,
+        app_client_id=config.CLIENT_ID
+    )
+
 
 
 def lambda_handler(event, context):
