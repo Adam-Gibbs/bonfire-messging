06ef66c80a0820fe70601575bd4f84d8b4adb4af
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-06 23:51:23 +0100

    redo decode again

--- endpoints/decode.py
+++ endpoints/decode.py
@@ -1,17 +1,21 @@
-import cognitojwt
 from endpoints.helpers.getData import get_header_auth
 from endpoints.helpers.returns import generate_response
-import endpoints.helpers.config as config
+import json
+from base64 import urlsafe_b64decode
+
+
+def maybe_pad(s):
+    return (s + '=' * (4 - len(s) % 4))
 
 
 def decode_token(token):
-    return cognitojwt.decode(
-        token,
-        config.USER_POOL_LOC,
-        config.USER_POOL_ID,
-        app_client_id=config.CLIENT_ID
-    )
-
+    _, payload, _ = token.split(".")
+    payload_json_str = urlsafe_b64decode(maybe_pad(payload))
+    payload_json = json.loads(payload_json_str, 'utf-8')
+    return {
+        "username": payload_json["username"],
+        "email": payload_json["email"]
+    }
 
 
 def lambda_handler(event, context):
