3128c089866ede9e54cead2faaacd469edf070db
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-07 00:16:04 +0100

    print

--- endpoints/decode.py
+++ endpoints/decode.py
@@ -12,6 +12,7 @@ def decode_token(token):
     _, payload, _ = token.split(".")
     payload_json_str = urlsafe_b64decode(maybe_pad(payload))
     payload_json = json.loads(payload_json_str)
+    print(payload_json)
     return {
         "username": payload_json["username"],
         "email": payload_json["email"]
