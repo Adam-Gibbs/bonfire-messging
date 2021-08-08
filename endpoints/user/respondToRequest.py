0760d14a13395fd7bdabd38a78a43f4ae28d9809
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-08 19:24:29 +0100

    respond to friend request

---
+++ endpoints/user/respondToRequest.py
@@ -0,0 +1,51 @@
+from endpoints.helpers.getRequestData import get_body, required_fields
+from endpoints.user_methods.getUsername import get_username
+from endpoints.helpers.returns import generate_response
+from endpoints.exceptions import handle_exception
+import boto3
+import os
+
+
+def lambda_handler(event, context):
+    params = get_body(event)
+    current_user = get_username(event)
+    client_db = boto3.client('dynamodb')
+
+    invalid_fields = required_fields(["friendRequestId", "accepted"], event)
+    if invalid_fields is not None:
+        return invalid_fields
+
+    friend_request_id = params['friendRequestId']
+    accepted = params['accepted']
+
+    try:
+        if accepted is True:
+            other_user = client_db.get_item(
+                TableName=os.environ['FRIEND_REQUESTS_TABLE'],
+                Key={
+                    'friendRequestId': {'S': friend_request_id}
+                }
+            ).get('Items').get("requestFrom")
+
+            client_db.put_item(
+                TableName=os.environ['FRIENDS_TABLE'],
+                Item={
+                    'friendLinkId': {'S': current_user+other_user},
+                    'friendTo': {'S': other_user},
+                    'friendOf': {'S': current_user}
+                }
+            )
+
+        client_db.delete_item(
+            TableName=os.environ['FRIEND_REQUESTS_TABLE'],
+            Key={
+                'friendRequestId': {'S': friend_request_id}
+            }
+        ).get('Items').get("requestFrom")
+
+        return generate_response(200, {
+            "success": True
+        })
+
+    except Exception as e:
+        return handle_exception(e)
