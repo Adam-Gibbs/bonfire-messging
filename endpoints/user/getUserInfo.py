af17ec4dfea35a3c8bf739a1d23e7b23f870ef6a
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-08 15:25:43 +0100

    fix exception handling

--- endpoints/user/getUserInfo.py
+++ endpoints/user/getUserInfo.py
@@ -1,13 +1,18 @@
 from endpoints.user_methods.getUsername import get_username
 from endpoints.user_methods.getEmail import get_email
 from endpoints.helpers.returns import generate_response
+from endpoints.auth.authExceptions import handle_auth_exception
 
 
 def lambda_handler(event, context):
-    username = get_username(event)
-    email = get_email(username)
+    try:
+        username = get_username(event)
+        email = get_email(username)
 
-    return generate_response(200, {
-        "username": username,
-        "email": email
-    })
+        return generate_response(200, {
+            "username": username,
+            "email": email
+        })
+
+    except Exception as e:
+        return None, handle_auth_exception(e)
--- endpoints/user_methods/getEmail.py
+++ endpoints/user_methods/getEmail.py
@@ -1,19 +1,14 @@
 import boto3
 
-import endpoints.auth.authExceptions as authExceptions
 import endpoints.helpers.config as config
 
 
 def get_email(username):
     client_cognito = boto3.client('cognito-idp')
 
-    try:
-        client_cognito.list_users(
-            AttributesToGet=["email"],
-            Filter=f'username = "{username}"',
-            Limit=1,
-            UserPoolId=config.config.USER_POOL_ID
-        )
-
-    except Exception as e:
-        return authExceptions.handle_auth_exception(e)
+    return client_cognito.list_users(
+        AttributesToGet=["email"],
+        Filter=f'username = "{username}"',
+        Limit=1,
+        UserPoolId=config.USER_POOL_ID
+    )
