65a10f6a877c8a6b151104e7811c3a0bcd4984f0
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-08 15:02:04 +0100

    fix import

--- endpoints/user/getUserInfo.py
+++ endpoints/user/getUserInfo.py
@@ -1,6 +1,6 @@
 from endpoints.user_methods.getUsername import get_username
 from endpoints.user_methods.getEmail import get_email
-from helpers.returns import generate_response
+from endpoints.helpers.returns import generate_response
 
 
 def lambda_handler(event, context):
