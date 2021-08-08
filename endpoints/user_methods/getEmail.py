ce7876a16c86fc7cad9cdbf549b0553916ddf468
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-08 15:33:08 +0100

    print result

--- endpoints/user_methods/getEmail.py
+++ endpoints/user_methods/getEmail.py
@@ -6,9 +6,13 @@ import endpoints.helpers.config as config
 def get_email(username):
     client_cognito = boto3.client('cognito-idp')
 
-    return client_cognito.list_users(
+    resp = client_cognito.list_users(
         AttributesToGet=["email"],
         Filter=f'username = "{username}"',
         Limit=1,
         UserPoolId=config.USER_POOL_ID
     )
+
+    print(resp)
+
+    return resp["username"]
