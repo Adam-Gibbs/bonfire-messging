63dce596d3c936a585a4adda72a30b4d942a867c
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-08 21:15:40 +0100

    fix get friends

--- endpoints/user/getFriends.py
+++ endpoints/user/getFriends.py
@@ -10,19 +10,38 @@ from endpoints.user_methods.getUsername import get_username
 def lambda_handler(event, context):
     current_user = get_username(event)
     client_db = boto3.client('dynamodb',  region_name=config.USER_POOL_LOC)
-    query = 'friendTo = :currentUser OR friendOf = :currentUser'
 
     try:
-        resp = client_db.query(
+        resp1 = client_db.query(
             TableName=os.environ['FRIENDS_TABLE'],
-            KeyConditionExpression=query,
+            KeyConditionExpression='friendTo = :currentUser',
             ExpressionAttributeValues={
                 ':currentUser': {'S': current_user}
             }
         )
 
+        resp2 = client_db.query(
+            TableName=os.environ['FRIENDS_TABLE'],
+            KeyConditionExpression='friendTo = :currentUser',
+            ExpressionAttributeValues={
+                ':currentUser': {'S': current_user}
+            }
+        )
+
+        if 'Items' in resp1:
+            resp1_items = resp1.get("Items")
+        else:
+            resp1_items = {}
+
+        if 'Items' in resp2:
+            total = resp2.get("Items")
+        else:
+            total = {}
+
+        total.append(resp1_items)
+
         return generate_response(200,  {
-            "resp": resp.get('Items')
+            "resp": total
         })
 
     except Exception as e:
