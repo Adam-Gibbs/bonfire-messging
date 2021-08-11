68568614ea97abbd12cf7f6db61ad0ba73495fbe
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-11 23:34:50 +0100

    remove prints

--- endpoints/fire/get.py
+++ endpoints/fire/get.py
@@ -48,11 +48,8 @@ def get_invited_fires(user, client_db):
         }
     )
 
-    print(f"invited {invited} \n")
-
     if 'Items' in invited:
         for item in invited.get("Items"):
-            print(f"item {item['fireId']['S']} \n")
             retrieved = client_db.get_item(
                 TableName=os.environ['FIRES_TABLE'],
                 Key={
@@ -60,7 +57,6 @@ def get_invited_fires(user, client_db):
                 }
             )
 
-            print(f"retrieved {retrieved} \n")
             if 'Item' in retrieved:
                 invited_fires.append(retrieved.get('Item'))
 
