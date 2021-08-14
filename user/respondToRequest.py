41ef152c665898a2e4c0c9fe5d9f080ce5613ffc
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-14 21:18:17 +0100

    to str

--- user/respondToRequest.py
+++ user/respondToRequest.py
@@ -38,7 +38,7 @@ def lambda_handler(event, context):
                     "message": "This friend request does not exist"
                 })
 
-            if current_user != response["Item"]["requestTo"]["S"]:
+            if current_user != str(response["Item"]["requestTo"]["S"]):
                 return generate_response(400, {
                     "success": False,
                     "message": "This friend request does not apply to you"
