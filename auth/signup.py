c0c89483578b93b8f8fe3d48fe28086313530420
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-15 16:13:16 +0100

    username

--- auth/signup.py
+++ auth/signup.py
@@ -33,7 +33,7 @@ def lambda_handler(event, context):
                     'Value': email
                 },
                 {
-                    'Name': "custom:username",
+                    'Name': "username",
                     'Value': username
                 }
             ],
