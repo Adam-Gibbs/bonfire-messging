1249556e020803bec2874bce4f6f766901a4ead0
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-15 20:56:24 +0100

    config

--- helpers/config.py
+++ helpers/config.py
@@ -1,3 +1,3 @@
 USER_POOL_LOC = 'eu-west-2'
-USER_POOL_ID = 'eu-west-2_OLbVrbhK3'
-CLIENT_ID = '6b45r3d1459pu9bcu5dvlu4grc'
+USER_POOL_ID = 'eu-west-2_YQtW4ZRzW'
+CLIENT_ID = '4rb0v3s6tuit95boqdafuro9h9'
