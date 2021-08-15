97b706c803843e1a1f5d21f922c246e4d9eb5e1a
Adam Gibbs <adam-gibbs@outlook.com>
2021-08-15 01:30:34 +0100

    updata cognito config

--- helpers/config.py
+++ helpers/config.py
@@ -1,3 +1,3 @@
 USER_POOL_LOC = 'eu-west-2'
-USER_POOL_ID = 'eu-west-2_xUNInjJwa'
-CLIENT_ID = '8gjhsrum4alfi1u9i6prargi2'
+USER_POOL_ID = 'eu-west-2_OLbVrbhK3'
+CLIENT_ID = '6b45r3d1459pu9bcu5dvlu4grc'
